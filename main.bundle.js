e908e2a0f3a76443c3d81003b2084831f99dc548
vladimir <logovazzik@gmail.com>
2017-10-20 19:54:57 +0300

    1

--- main.bundle.js
+++ main.bundle.js
@@ -66,7 +66,7 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 
 var AppComponent = (function () {
     function AppComponent(windowRef, utils) {
-        var a2ajx = windowRef.nativeWindow.location.search.replace(/^\?/, '');
+        var a2ajx = utils.ajx2q(windowRef.nativeWindow.location.search.replace(/^\?/, ''));
         this.userID = a2ajx[__WEBPACK_IMPORTED_MODULE_3__app_constants__["b" /* userKeyUrl */]];
         this.authKey = a2ajx[__WEBPACK_IMPORTED_MODULE_3__app_constants__["a" /* userAuthKeyUrl */]];
     }
