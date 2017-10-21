3cab8e18def7a16cccb1b6d2a980844371ff48bd
vladimir <logovazzik@gmail.com>
2017-10-22 02:14:53 +0300

    wwd

--- src/app/components/find-page.component/find-page.component.ts
+++ src/app/components/find-page.component/find-page.component.ts
@@ -1,4 +1,4 @@
-import {Component, ChangeDetectionStrategy, ChangeDetectorRef} from '@angular/core';
+import {Component, ChangeDetectionStrategy, ChangeDetectorRef, OnDestroy} from '@angular/core';
 import {ApiService} from "../../services/api.service";
 import {HackinderService} from "../../services/hackinder.service";
 
@@ -11,20 +11,24 @@ import {HackinderService} from "../../services/hackinder.service";
     'style': 'height: 100%; display: block'
   }
 })
-export class FindPageComponent{
+export class FindPageComponent implements OnInit, OnDestroy{
   constructor(private hackinderService: HackinderService, private _ref: ChangeDetectorRef){
   }
   public possibleMatches: any[] = [];
   public like(){
-    this.hackinderService.doLike(this.possibleMatches[0].user_id).subscribe()
+    this.hackinderService.doLike(this.possibleMatches[0].user_id);
     this.possibleMatches.pop();
   }
+  private _subscriptions: any = [];
   ngOnInit(){
     this.hackinderService.getPossibleMatches();
-    this.hackinderService.possibleMatches$.subscribe((matches)=>{
+    this._subscriptions.push(this.hackinderService.possibleMatches$.subscribe((matches)=>{
       this.possibleMatches =  this.possibleMatches.concat(matches);
       this._ref.detectChanges();
-    });
+    }));
+  }
+  ngOnDestroy(){
+    this._subscriptions.map((item)=> item.unsubscribe(); )
   }
 }
 
