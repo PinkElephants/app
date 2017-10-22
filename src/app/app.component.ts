28a12a2570d26e84a96e0baa64c42a52c847a1ef
vladimir <logovazzik@gmail.com>
2017-10-22 07:16:31 +0300

    ewfwefwefwef

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import {WindowRefService} from './services/window.service';
 import { UtilsService} from './services/utils.service';
 import {userAuthKeyUrl, userKeyUrl} from './app.constants';
 import {AuthService} from "./services/auth.service";
 import {HackinderService} from "./services/hackinder.service";
+import {NavigationEnd, Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -14,16 +15,23 @@ import {HackinderService} from "./services/hackinder.service";
     'style': 'height: 100%; display: block',
   }
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public userID: string;
   public authKey: string;
-  constructor(windowRef: WindowRefService, utils: UtilsService, auth: AuthService, hack: HackinderService) {
+  constructor(private windowRef: WindowRefService, private router: Router, utils: UtilsService, auth: AuthService, hack: HackinderService) {
     const q2ajx = utils.q2ajx(<any>windowRef.nativeWindow.location.search.replace(/^\?/, ''));
     this.userID = q2ajx[userKeyUrl];
     this.authKey = q2ajx[userAuthKeyUrl];
     this.authKey = '3698302d5f1b2b85ad948ecbbdf18db0';
     this.userID = '667976';
     auth.storeCredentials(this.authKey, this.userID);
-
   }
+    ngOnInit(){
+      this.router.events.subscribe((evt) => {
+        if (!(evt instanceof NavigationEnd)) {
+          return;
+        }
+        this.windowRef.nativeWindow.scrollTo(0, 0);
+      });
+    }
 }
