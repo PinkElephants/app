3f7129cf26684c3103921ae4f953b0e23bd2361b
vladimir <logovazzik@gmail.com>
2017-10-22 07:12:04 +0300

    wefwefwef

--- src/app/components/find-page.component/find-page.component.ts
+++ src/app/components/find-page.component/find-page.component.ts
@@ -17,8 +17,6 @@ export class FindPageComponent implements OnInit, OnDestroy{
 
   public possibleMatches: any[] = [];
   public like(){
-    console.log(this.possibleMatches[0].user_id);
-    console.log(this.possibleMatches[0]);
     this.hackinderService.doLike(this.possibleMatches[0].user_id).subscribe();
     this.possibleMatches.shift();
   }
